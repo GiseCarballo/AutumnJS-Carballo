58c62d9f4ddbe269df61e9d670a0427d5ea0939b
GiseCarballo <giselacarballo3@gmail.com>
2022-11-05 09:55:56 -0300

    Armando la calcuadora de fechas

--- JS/codigo-calendario.js
+++ JS/codigo-calendario.js
@@ -13,22 +13,30 @@ function crearTarjetas(){
             <div class="card-body">
                     <h5 class="card-title">${juego.nombre}</h5>
                     <p class="card-text">Código: ${juego.id}</p>
-                    <button id='btn${juego.dias}' class="btn botonTiempo">Ver tiempo</button>
+                    <button id='btn${juego.id}' class="btn botonTiempo">Ver tiempo</button>
             </div>
         `;
         tarjeta.append(tarjetaDiv);
   
 }) 
+
+    juegos.forEach((juego)=>{
+        document.getElementById(`btn${juego.id}`).addEventListener("click",function(){
+        verTiempo(juego);
+        });
+    });
+
+ 
 }
-crearTarjetas();
+crearTarjetas();   
 
 //EVENTOS
 //Sweet Alert
 
 function verTiempo(juegoAVisualizar){
-    Swal.fire({
+      Swal.fire({
             title: juegoAVisualizar.nombre,
-            text: 'El producto que adquiriste tarda '+juegoAVisualizar.dias+' días en estar listo',
+            text: 'El producto "'+juegoAVisualizar.nombre+ '", tarda '+juegoAVisualizar.dias+' días en estar listo',
             imageUrl: juegoAVisualizar.foto,
             imageWidth: 200,
             imageHeight: 200,
@@ -37,26 +45,19 @@ function verTiempo(juegoAVisualizar){
 }
 
 
-juegos.forEach((juego)=>{
-    document.getElementById(`btn${juego.dias}`).addEventListener("click",function(){
-        verTiempo(juego);
-    });
-});
-
-
 
 // VALINDANDO FORMULARIO
 
 
 const nombreUsuario = document.getElementById("nombre");
-const productos = document.getElementById("seleccionadorProd");
+const select = document.getElementById("seleccionadorProd");
 const fechaCompra = document.getElementById("fecha");
 
 function enviarFormulario (){
     if (nombreUsuario.value === null || nombreUsuario.value === ""){
         console.log("No ingresó su nombre");
     }
-    if (productos.value === null || productos.value === ""){
+    if (select.value === null || select.value === ""){
         console.log("No seleccionó un producto");
     }
     if (fechaCompra.value === null || fechaCompra.value === ""){
@@ -76,9 +77,19 @@ window.addEventListener('load', function () {
 // Funcion del boton "Calcular fecha" - trabajar con Luxon e imput de fecha + juego.dias
 //function calcularFecha ()
 
-let opcion = document.getElementsByTagName("option");
-const DateTime = luxon.DateTime
 
+const DateTime = luxon.DateTime
+const dur10 = Duration.fromObject ({ days:10 });
+const dur15  = Duration.fromObject ({ days:15 });
+const dur20 = Duration.fromObject ({ days:20 });
+
+function calcularFecha (
+
+)
+
+
+const fechaListo = fechaCompra.plus(dur10);
+console.log(suma.toLocaleString(DateTime.DATE_SHORT))
 
 //const fechaCompra = DateTime.now(document.getElementById("Compra"))
 //const fechaListo = fechaCompra.plus(${juego.dias});
