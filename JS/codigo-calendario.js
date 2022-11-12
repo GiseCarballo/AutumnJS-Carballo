cf26fd67d4ee5a330af49eb98fb8df4f123892ad
GiseCarballo <giselacarballo3@gmail.com>
2022-11-12 14:01:36 -0300

    Chequeando la función para Calcular la Fecha

--- JS/codigo-calendario.js
+++ JS/codigo-calendario.js
@@ -1,7 +1,7 @@
 // Todo el codigo de calendario.html
 
 
-//cards
+//CARDS - Tarjetas informativas para que el cliente sepa cuándo tarda cada producto.
 let tarjeta = document.getElementById("tarjetas");
 
 function crearTarjetas(){
@@ -14,24 +14,20 @@ function crearTarjetas(){
                     <h5 class="card-title">${juego.nombre}</h5>
                     <p class="card-text">Código: ${juego.id}</p>
                     <button id='btn${juego.id}' class="btn botonTiempo">Ver tiempo</button>
-            </div>
-        `;
+            </div>`;
         tarjeta.append(tarjetaDiv);
-  
-}) 
+        }) 
 
-    juegos.forEach((juego)=>{
-        document.getElementById(`btn${juego.id}`).addEventListener("click",function(){
-        verTiempo(juego);
+        juegos.forEach((juego)=>{
+            document.getElementById(`btn${juego.id}`).addEventListener("click",function(){
+            verTiempo(juego);
         });
     });
-
- 
 }
 crearTarjetas();   
 
 //EVENTOS
-//Sweet Alert
+//Sweet Alert para que el cliente pueda saber lo que tarda cada producto.
 
 function verTiempo(juegoAVisualizar){
       Swal.fire({
@@ -41,11 +37,13 @@ function verTiempo(juegoAVisualizar){
             imageWidth: 200,
             imageHeight: 200,
             imageAlt: juegoAVisualizar.nombre,
-          })
-}
-
+          });
+};
 
 // SELECT
+// Armar un select que esté asociado directamente con los objetos del array para luego generar la función de CalcularFecha,
+//y para que se modifique cada vez que sea necesario modificar el array.
+
 const selectProd = document.getElementById("selectProd");
 
 function seleccionarProd (){
@@ -56,18 +54,12 @@ function seleccionarProd (){
         `;
         selectProd.append();
     });
-
-
-}
+};
 seleccionarProd();
 
 
 //ONSUBMIT
 /*
-- Escuchar el submit del formulario con un onSubmit.
-- En ese momento, necesitamos obtener 1. el value del producto seleccionado en el select de productos, con su ID.
--También necesitamos la fecha, como objeto Date.
--Con el ID del producto, buscamos con un find el producto en tu lista de juegos y obtenemos la demora del envío.
 -Utilizamos la función que te pasé para sumarle esa cantidad de días a la fecha que ingresó el usuario y listo, tendríamos la fecha estimada de envío.
 -Entonces obtenemos ese ID del value, iteramos sobre el array de objetos, cuando coincide el ID del FORM con el ID del producto, devolvemos los días de demora.
 
@@ -83,7 +75,7 @@ const fechaCompra = document.getElementById("fecha");
 function enviarFormulario (e){
     if (nombreUsuario.value === null || nombreUsuario.value === ""){
         e.preventDefault();
-        console.log("No ingresó su nombre");
+        console.log("No ingresó un nombre");
     }
     if (opcionElegida.value === null || opcionElegida.value === ""){
         e.preventDefault();
@@ -104,18 +96,29 @@ window.addEventListener('load', function () {
 
 
 //ON SUBMIT
+//Escuchar el select y la fecha para poder trabajar en la función para calcular la fecha.
 
 let miBoton = document.getElementById("miBoton");
 
 const formulario = document.getElementById("formulario");
 
+    formulario.addEventListener("submit", (e) => {  
+        e.preventDefault(); //Para que no se recargue la página
+        const idSolicitado =
+        opcionElegida.options[opcionElegida.selectedIndex].value;
+        const productoSolicitado = juegos.find((juego) => juego.id == idSolicitado);
+       
+       
+        function CalcularFecha(){
+        let date = new Date(fechaCompra.valueAsDate.getTime());
+        date.setDate(date.getDate() + productoSolicitado.dias);
+        return date; //Acá debería haber un Sweet Alert que diga "Tu producto estará listo el NuevaFecha"
+        };
+        console.log(productoSolicitado.dias); //para chequear si ve los días que tarda
 
-formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const idSolicitado = opcionElegida.options[opcionElegida.selectedIndex].value;
-    const productoSolicitado = juegos.find((juego) => juego.id == idSolicitado);
-    
-});
+
+        CalcularFecha();
+    });
 
 
 
--- JS/codigo-user.js
+++ JS/codigo-user.js
@@ -1,6 +1,6 @@
 //Todo el codigo de index.html
 
-// Solicitud de Usuario y contraseña para acceder al calendario
+// Solicitud de Usuario y contraseña para acceder al calendario (Simulador)
 
 let email = document.getElementById("email");
 let contrasenia = document.getElementById("pass");
@@ -18,21 +18,30 @@ formulario.addEventListener("submit", (e) => {
         } else {
             intentos -= 1;
             Swal.fire("Usuario y/o contraseña erroneos! Restan " + (intentos) + " intentos.");
-        }
-     }
-    
+        }}
 });
 
 
-email.oninput = () => {
+/*email.oninput = () => {
     if(isNaN(email.value)){
         email.style.color="#ffffff";
     }else{
         email.style.color="red";
     }
-}
+}*/
 
+// función para que el email contenga su estructura básica, más allá de haberlo declarado en el simulador
+function validarEmail (){
+    let validacionDeEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    if(validacionDeEmail.test(email.value)){
+        email.style.color="#ffffff";
+        console.log('Email escrito correctamente');
+		return true;
+	}else{
+        email.style.color="red";
+		console.log('El Email contiene un error en su escritura');
+		return false;
+	}
+}   
 
-
-
-
+validarEmail();
